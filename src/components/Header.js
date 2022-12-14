91bdc5d10c49b8ac54941469476f0bad239d2450
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-12-14 16:47:11 -0300

    requisito 03 concluido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends Component {
+  state = {
+    user: '',
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      this.setState({ loading: false, user: user.name });
+    });
+  }
+
   render() {
+    const { user, loading } = this.state;
     return (
       <header data-testid="header-component">
-
+        {loading ? <Loading /> : <p data-testid="header-user-name">{user}</p>}
         <Link to="/search" data-testid="link-to-search">
           Search
         </Link>
